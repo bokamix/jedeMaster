e1dddbe38a1b989e0588354dd82e9b52741ec9b4
bokamix <janiakpol@gmail.com>
2019-11-30 17:38:59 +0100

    Repush

--- src/components/AboutChallenge.js
+++ src/components/AboutChallenge.js
@@ -1,53 +1,47 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import styled from "styled-components";
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import { loadState } from '../localStorage'
-
+import React from "react"
+import styled from "styled-components"
+import { loadState } from "../localStorage"
 
 const MainWrapper = styled.div`
-width:70%;
-margin: 0 auto;
-margin-top:100px;
-display:flex;
-flex-wrap:wrap;
-
+  width: 70%;
+  margin: 0 auto;
+  margin-top: 100px;
+  display: flex;
+  flex-wrap: wrap;
 `
 const Papper = styled.div`
-margin:30px;
-width:100%;
+  margin: 30px;
+  width: 100%;
 
-border-radius:30px;
-padding:30px;
-div{
-  display:flex;
-  margin:20px;
-  width:100%;
-  p{
-    margin:10px;
+  border-radius: 30px;
+  padding: 30px;
+  div {
+    display: flex;
+    margin: 20px;
+    width: 100%;
+    p {
+      margin: 10px;
+    }
   }
-}
-
 `
 
-
-
 export default function AboutChallenge() {
-  const [logItems, setlogItems] = React.useState(loadState('dayLogs'))
+  const [logItems, setlogItems] = React.useState(loadState("dayLogs"))
+
+  // const logItemsMap = () => {
+  //   if(loadState("dayLogs")){logItems.map((item, num) => {
+  //     return (
+  //       <div key={num}>
+  //         <p>{item.date}</p>
+  //         <p>{item.isDone}</p>
+  //       </div>
+  //     )
+  //   })}
+  // }
+
   return (
     <MainWrapper>
-   <Papper>
-{logItems.map((item,num)=>{
-
-console.log(item)
-return(
-  <div key={num}>
-<p>{item.date}</p><p>{item.isDone}</p>
-  </div>
-)
-})}
-</Papper>
+      <Papper></Papper>
     </MainWrapper>
-  );
+  )
 }
