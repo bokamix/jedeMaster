5ff9a4f1f6bb8e0ffd078c913fdcda1ae012bb37
bokamix <wjaniak@jede.pl>
2020-01-12 16:06:45 +0100

    repair netlify auth

--- src/pages/login.js
+++ src/pages/login.js
@@ -6,6 +6,8 @@ import SEO from "../components/seo"
 import { loadState } from "../localStorage"
 import { getFirebase } from "../../firebase"
 
+let netlifyIdentity
+
 function initNetlifyIdentity() {
   console.log("initNetlifyIdentity called.")
   const script = document.createElement("script");
@@ -13,37 +15,39 @@ function initNetlifyIdentity() {
   script.async = true;
   document.body.appendChild(script);
 }
-let netlifyIdentity
+
+function openNetlifyModal() {
+  const netlifyIdentity = window.netlifyIdentity;
+
+  if(netlifyIdentity)
+    netlifyIdentity.open();
+  else
+    console.log('netlifyIdentity not defined')
+}
 
 //////
 const NetlifyIdentity = () => {
   useEffect(() => {
-    netlifyIdentity = window.netlifyIdentity;
     initNetlifyIdentity();
+    console.log("iniet Net... End")
+
+
     const lazyApp = import('firebase/app')
     const lazyDatabase = import('firebase/database')
     Promise.all([lazyApp, lazyDatabase]).then(([firebase]) => {
       const database = getFirebase(firebase).database()
       let data = {
-        dada: "duka blat",
+        dada: "Repairt net duka blat",
       }
       database.ref().set(data);
     })
   });
-  function openNetlifyModal() {
-    if(netlifyIdentity){
-      netlifyIdentity.open();
-    }
-    else
-      console.log('netlifyIdentity not defined')
-  }
-
 
   return(
     <Layout>
       <SEO title="Home" />
       <h1>Hi people</h1>
-      <h2 onClick={openNetlifyModal}>Login</h2>
+      <h2 onClick={()=>openNetlifyModal()}>Login</h2>
       <p>Welcome to your new Gatsby site.</p>
       <p>Now go build something great.</p>{console.log("asdad" ,loadState('gotrue.user'))}
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
