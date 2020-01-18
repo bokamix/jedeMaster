c7204b9ff1f8e7a7bb6ef999469398ccdeb300c4
bokamix <wjaniak@jede.pl>
2020-01-18 09:20:01 +0100

    Change kolejność

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react"
+import { saveToFire, loadFromFire } from "../../firebase"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import MenuPanel from "../components/Menu/MenuPanel";
@@ -7,7 +8,6 @@ import ContentWrapper from "../components/ContentWrapper"
 import { loadState } from '../localStorage'
 import FirstTimeUser from "../components/FirstTimeOnApp/FirstTimeUser"
 import  "../../firebase"
-import { saveToFire, loadFromFire } from "../../firebase"
 
 
 
